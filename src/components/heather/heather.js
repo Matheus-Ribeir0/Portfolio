f10ae40a36991d7c3832aa75e8824f4493005eba
Matheus Ribeiro <matheus.tex02@gmail.com>
2024-10-05 15:19:37 -0300

    first commit

---
+++ src/components/heather/heather.js
@@ -0,0 +1,12 @@
+import "./heather.css"; 
+
+const Hether = (props) => {
+    return (
+        <header>
+            <h1>Hello World</h1>
+        </header>
+    );
+
+}
+
+export default Hether;
---
+++ src/components/mainPage/mainPage.js
@@ -0,0 +1,11 @@
+import "./mainPage.css"; 
+const MainPage = (props) => {
+    return (
+        <div className="main_page">
+            <h1>Hello World</h1>
+        </div>
+    );
+
+}
+
+export default MainPage;
